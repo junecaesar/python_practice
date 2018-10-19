0b64a456662d2e1293e7e73d36807d27b4f47952
junecaesar <junecaesar@yahoo.com>
2018-10-19 22:43:27 +0800

    Fibonacci sequence running time

--- practice6.py
+++ practice6.py
@@ -1,21 +1,20 @@
 #题目：斐波那契数列 Fibonacci sequence
 
-#input the amount you wantto make
+#input the amount of numbers you wanna make
 import time
 
 print("Please input the end point of the Fibonacci sequence:")
 end_point=int(input())
 if end_point>1000000:
-   print("toooo large")
+   print("tooo large")
    exit()
 a=0;b=1;i=0
 start_time=time.time()
 
-while i<end_point:
+for i in range(end_point):
     tmp=a+b
     a=b
     b=tmp
-    i+=1
 
 """    print(a, ' ', end='')
     if(i%20)==0:
