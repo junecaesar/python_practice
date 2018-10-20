adf260ad1194d661074cb9b41d12c530979a0420
junecaesar <junecaesar@yahoo.com>
2018-10-20 22:06:19 +0800

    Fibonacci sequence running time v2

--- practice6.py
+++ practice6.py
@@ -8,18 +8,22 @@ end_point=int(input())
 if end_point>1000000:
    print("tooo large")
    exit()
-a=0;b=1;i=0
+a=0;b=1;
 start_time=time.time()
 
-for i in range(end_point):
-    tmp=a+b
-    a=b
-    b=tmp
-
-"""    print(a, ' ', end='')
-    if(i%20)==0:
+for i in range(1,end_point):
+    print(' %12ld %12ld'%(a,b), end='')
+    a=a+b
+    b=a+b
+    if(i%5)==0:
         print()
-"""
 
 end_time=time.time()
-print("\nstart:",start_time,"\tEnd:",end_time,"\nRun time(in seconds):",end_time-start_time,"\nend point")
+print("\n\nstart:",time.strftime('%Y-%M-%D %H:%M:%S',time.localtime(start_time)),"\tEnd:",
+      time.localtime(end_time),"\nRun time(in seconds):",end_time-start_time,"\nend point")
+'''
+myD = {1: 'a', 2: 'b'}
+for key, value in dict.items(myD):
+    print(key, value)
+    time.sleep(1) # 暂停 1 秒
+'''
