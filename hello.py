61e076f648ff806cb5cec7f3b07c9b32f03c58e5
junecaesar <junecaesar@yahoo.com>
2018-10-21 18:01:56 +0800

    calculate the speed

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-
+import time
 print("Hello Python")
 text="why are we here"
 print(text)
+
+total=0
+print("please input:",end='')
+end_num=int(input())
+
+start_time=time.time()
+for i in range(end_num):
+    total=total+i
+
+end_time=time.time()
+print("\nstart:",time.strftime('%Y-%M-%D %H:%M:%S',time.localtime(start_time)),"\tEnd:",
+      time.localtime(end_time),"\nRun time(in seconds):",end_time-start_time,"\nanswer is:",total)
