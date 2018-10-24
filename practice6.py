2a050fcc0a89fd03709080a4d7c074be66ec7ff7
junecaesar <junecaesar@yahoo.com>
2018-10-24 21:14:58 +0800

    use datetime

--- practice6.py
+++ practice6.py
@@ -1,7 +1,8 @@
 #题目：斐波那契数列 Fibonacci sequence
 
 #input the amount of numbers you wanna make
-import time
+#import time
+import datetime
 
 print("Please input the end point of the Fibonacci sequence:")
 end_point=int(input())
@@ -9,18 +10,21 @@ if end_point>1000000:
    print("tooo large")
    exit()
 a=0;b=1;
-start_time=time.time()
+#start_time=time.time()
+start_time=datetime.datetime.now()
+
 
 for i in range(1,end_point):
-    print(' %12ld %12ld'%(a,b), end='')
+#    print(' %12ld %12ld'%(a,b), end='')
+#    if(i%5)==0:
+ #       print()
     a=a+b
     b=a+b
-    if(i%5)==0:
-        print()
 
-end_time=time.time()
-print("\n\nstart:",time.strftime('%Y-%M-%D %H:%M:%S',time.localtime(start_time)),"\tEnd:",
-      time.localtime(end_time),"\nRun time(in seconds):",end_time-start_time,"\nend point")
+#end_time=time.time()
+end_time=datetime.datetime.now()
+
+print("The start time is:%s End time is:%s Running time is:%s"%(start_time,end_time,end_time-start_time))
 '''
 myD = {1: 'a', 2: 'b'}
 for key, value in dict.items(myD):
